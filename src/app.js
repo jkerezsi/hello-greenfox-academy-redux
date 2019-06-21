00aef2f1da04da6ef79e27ddeb850ae9acca30a4
Janos <kerezsi1987@gmail.com>
2019-06-21 09:51:15 +0200

    feat(hello-greenfox-redux)

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BuyAcorn from './containers/buyAcorn';
+import EatAcorn from './containers/eatAcorn';
+import DisplayAcorn from './containers/displayAcorn';
+
+const App = () => (
+  <main>
+    <BuyAcorn />
+    <DisplayAcorn />
+    <EatAcorn />
+  </main>
+);
+
+export default App;
